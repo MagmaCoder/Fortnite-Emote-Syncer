242753f3b31ff811df505350bb7354671f842c17
MagmaCoder <30716007+MagmaCoder@users.noreply.github.com>
2022-06-13 23:08:11 +0100

    updated gitignore and server client handling

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import threading
 import socket
 from datetime import datetime
 import os
-
+import uuid
 HOST = "localhost"
 PORT = 65432
 
@@ -13,10 +13,24 @@ clients = {}
 def Client(clientsocket : socket.socket,addr):
     global clients
 
+    msg = b""
+
     clientsocket.setblocking(0)
 
     while allowConnections:
-        pass
+        if msg != b"":
+            pass
+        try:
+            #10035 is no data
+            #10054 is closed connection
+            msg = clientsocket.recv(1024)
+        except WindowsError as e:
+            if e.winerror == 10054:
+                print(f"Connection closed.")
+                clientsocket.close()
+                break
+    
+    clientsocket.close()
 
 def openSocket(host, port):
     conns = {}
